579dcc9a3be08b2d0a196e4c0e456307c5d0d672
shino-sky <ms_020916@yahoo.co.jp>
2020-02-16 15:59:00 +0900

    add BIT_2D

---
+++ BIT_2D.cpp
@@ -0,0 +1,22 @@
+struct BIT_2D {
+  const int h, w;
+  vector<vector<ll>> data;
+  BIT_2D(int h, int w): h(h), w(w), data(h+1, vector<ll>(w+1)) { }
+  void add(int r, int c, ll v) {
+    r++; c++;
+    for (; r <= h; r += r & -r)
+      for (; c <= w; c += c & -c)
+        data[r][c] += v;
+  }
+  // sum over [(0, 0), (r, c))
+  ll sum(int r, int c) {
+    ll res = 0;
+    for (; r > 0; r -= r & -r)
+      for (; c > 0; c -= c & -c)
+        res += data[r][c];
+  }
+  // sum over [(r1, c1), (r2, c2))
+  ll sum(int r1, int c1, int r2, int c2) {
+    return sum(r1, c1) + sum(r2, c2) - sum(r1, c2) - sum(r2, c1);
+  }
+}
