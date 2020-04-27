ae76fc483664a779089017ce0e26177b0d60913e
shino-sky <ms_020916@yahoo.co.jp>
2020-04-27 18:46:46 +0900

    add BIT test

---
+++ verify/BIT_2D.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2842"
+
+#include "template.cpp"
+#include "data-structure/BIT_2D.cpp"
+
+int main() {
+  int h = IN, w = IN, T = IN, q = IN;
+
+  queue<pair<int, PII>> trans;
+  BIT_2D A(h, w), B(h, w);
+
+  rep(q) {
+    int t = IN, cmd = IN, r = IN1, c = IN1;
+
+    while (not trans.empty() and trans.front().first <= t) {
+      PII p; tie(ignore, p) = trans.front(); trans.pop();
+      A.add(p.first, p.second, -1);
+      B.add(p.first, p.second, +1);
+    }
+
+    if (cmd == 0) {
+      A.add(r, c, +1);
+      trans.emplace(t+T, PII{r, c});
+    } else if (cmd == 1) {
+      if (B.sum(r, c, r+1, c+1))
+        B.add(r, c, -1);
+    } else {
+      int r2 = IN, c2 = IN;
+      OUT(B.sum(r, c, r2, c2), A.sum(r, c, r2, c2));
+    }
+  }
+}
---
+++ verify/BIT_compress.test.cpp
@@ -0,0 +1,39 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0343"
+
+#include "template.cpp"
+#include "util/compress.cpp"
+#include "data-structure/BIT.cpp"
+
+#define D_GLIBCXX_DEBUG
+
+int main() {
+  int n = IN, c = IN;
+
+  VI cs(c), ts(c); VLL ps(c);
+  rep(i, c) {
+    cs[i] = IN, ts[i] = IN;
+    if (cs[i] == 0) ts[i]--, ps[i] = IN;
+  }
+
+  VLL scores(n);
+  Compress<pair<ll, int>> comp;
+
+  rep(i, n) comp.emplace(scores[i], -i);
+  rep(i, c) if (cs[i] == 0)
+      comp.emplace(scores[ts[i]] += ps[i], -ts[i]);
+
+  fill(all(scores), 0);
+
+  BIT bit(comp.size());
+  rep(i, n) bit.add(comp[make_pair(0, -i)], 1);
+
+  rep(i, c) {
+    if (cs[i] == 0) {
+      bit.add(comp[make_pair(scores[ts[i]], -ts[i])], -1);
+      bit.add(comp[make_pair(scores[ts[i]] += ps[i], -ts[i])], 1);
+    } else {
+      int s = bit.upper_bound(n - ts[i]);
+      OUT(-comp.rev(s).second+1, comp.rev(s).first);
+    }
+  }
+}
