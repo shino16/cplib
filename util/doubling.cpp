4f799a47d9c37ff1a2086e55d8e57014ba853fb0
shino-sky <ms_020916@yahoo.co.jp>
2020-05-11 17:57:32 +0900

    split files

--- util/doubling.cpp
+++ util/doubling.cpp
@@ -1,35 +1,7 @@
 #pragma once
 
 #include "template.cpp"
-
-class Mapping {
- public:
-  struct Combine {
-    Mapping operator()(const Mapping& lhs, const Mapping& rhs) {
-      if (lhs.f.empty()) return rhs;
-      if (rhs.f.empty()) return lhs;
-      assert(lhs.f.size() == rhs.f.size());
-      int n = lhs.f.size();
-      vector<int> f(n);
-      rep(x, n) {
-        int y = rhs.f[x];
-        f[x] = 0 <= y and y < n ? lhs.f[y] : y;
-      }
-      return Mapping(move(f));
-    }
-  };
-
- private:
-  vector<int> f;
-
- public:
-  Mapping() = default;
-  Mapping(int n) : f(n) { iota(all(f), 0); }
-  Mapping(const vector<int>& f_) : f(f_) {}
-  Mapping(vector<int>&& f_) : f(move(f_)) {}
-
-  int operator()(int x) { return f[x]; }
-};
+#include "util/mapping.cpp"
 
 template <typename T = Mapping, typename Combine = typename T::Combine>
 class Doubling {
